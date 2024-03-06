aeb23f242f4af119725510f3d5fe79b399d3b280
baliharsha <baliharsha75@gmail.com>
2024-03-06 19:35:42 +0530

    update

--- src/Components/Quiz App/Quiz.jsx
+++ src/Components/Quiz App/Quiz.jsx
@@ -1,20 +1,88 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import  './Quiz.css'
+import { data } from '../../assets/data';
 
-function Quiz() {
-  return (
+const Quiz = () => {
+
+  let [index, setindex] = useState(0);
+  let [question,setquestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0)
+  let [result, setResult] = useState(false)
+
+  let option1 = useRef(null);
+  let option2 = useRef(null);
+  let option3 = useRef(null);
+  let option4 = useRef(null);
+  // let option5 = useRef(null);
+
+
+  let option_array = [option1, option2, option3, option4,];
+
+ 
+  const checkAns =(e,answer) => {
+     if(lock === false) {
+
+     if(question.answer===answer){
+      e.target.classList.add("correct");
+      setLock(true);
+      setScore(prev=>prev+1);
+    }
+    else{
+      e.target.classList.add("wrong");
+      setLock(true);
+      option_array[question.answer-1].current.classList.add("correct")
+    }
+   }
+  }
+  
+   const next = () => {
+     if (lock===true) {
+      if (index === data.length-1){
+          setResult(true);
+          return 0;
+      }
+      setindex(++index);
+      setquestion(data[index]);
+      setLock(false);
+      option_array.map((option)=>{
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+        return null;
+
+      })
+     }
+   }
+
+  const reset = () => {
+    setindex(0);
+    setquestion(data[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+  }
+
+ return (
     <div className='container'>
 <h1>Quiz App</h1>
 <hr/>
-<h2>1. What is you country name</h2>
+{result? <></>:<>
+ <h2>{index+1}. {question.question}</h2>
 <ul>
-    <li>india</li>
-    <li>chennai</li>
-    <li>bhutan</li>
-   <li>nyc</li>
+    <li ref={option1}   onClick={(e)=>{checkAns(e,1)}}>{question.options1}</li>
+    <li ref={option2}  onClick={(e)=>{checkAns(e,2)}}>{question.options2}</li>
+    <li ref={option3}  onClick={(e)=>{checkAns(e,3)}}>{question.options3}</li>
+    <li ref={option4}  onClick={(e)=>{checkAns(e,4)}}>{question.options4}</li>
+    {/* <li ref={option5}   onClick={(e)=>{checkAns(e,5)}}>{question.option5}</li> */}
+
    </ul>
-   <button>Next</button>
-   <div className='index'>1 of 5 question </div>
+   <button onClick={next}>Next</button>
+   <div className='index'> {index+1 }of {data.length} question </div> </>}
+   {result? <> 
+   <h2>Your Scored {score} out of {data.length}</h2>
+   <button onClick ={reset}>Reset</button>
+   </> : <></> }
+
   </div>
   )
 }
--- src/assets/Data.js
+++ src/assets/Data.js
@@ -1,14 +1,45 @@
-export const Data = [
+export const data = [
 {
-    question: "what is you name"
-    options1: "harsha",
-    options2: "rahul",
-    ans: 1,
+    question: " Which of the following is used in React.js to increase performance?",
+    options1: "Virtual DOM",
+    options2: "Original DOM", 
+    options3: "Both A and B" ,
+    options4: "None of the above", 
+    answer: 1,
+
 },
 {
-    question: "what is you name"
-    1: "harsha",
-    options2: "rahul",
-    ans: 2,
-}          
-]
+    question: "What is ReactJS?",
+    options1: "Server-side framework",
+    options2: "User interface framework", 
+    options3: "Both A and B" ,
+    options4: "None of the above", 
+    answer: 2,
+    
+},
+{
+    question: "Identify the one which is used to pass data to components from outside",
+    options1: "Render with arguments",
+    options2: "setState" ,
+    options3: "propTypes" ,
+    options4: "props" ,
+    answer: 4,
+},
+{
+    question: "Identify the command used to create a react app",
+    options1: "npm install create-react-app",
+    options2: "npm install -g create-react-app",  
+    options3: " install-g create-react-app", 
+    options4: "None of the above" ,
+    answer: 2,
+},
+{
+    question: "What is Babel?",
+    options1: "javascript compiler",
+    options2: "javascript interpreter", 
+    options3: "javascript transpiller", 
+    options4: "None of the above" ,
+    answer: 1,
+
+}   
+];
